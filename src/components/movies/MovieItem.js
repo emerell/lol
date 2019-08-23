ebe6f4703b48ed2494dc8c74885b73d2c572c589
Lera <valeriya.mihaleva.1999@gmail.com>
2019-08-23 23:24:27 +0300

    Added responsive styles for MovieItem component

--- src/components/movies/MovieItem.js
+++ src/components/movies/MovieItem.js
@@ -14,35 +14,38 @@ export class MovieItem extends Component {
         this.props.genres.filter(genre => movieGenresIds.includes(genre.id))
     )
 
-    getMovieReleaseYear = releaseDate => releaseDate.slice(0, 4)
+    getMovieReleaseYear = releaseDate => ` (${releaseDate.slice(0, 4)})`
 
     render() {
         const { title, poster_path, genre_ids, release_date } = this.props.movieItem;
         const genres = this.getMovieGenres(genre_ids);
+        const posterImageUrl = `https://image.tmdb.org/t/p/w342${poster_path}`
         return (
-            <article className="movie-card">
-                <a href="#" className="movie-card__poster-box">
-                    <div className="movie-card__background"
-                        style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w342${poster_path})` }}>
+            <div className="column">
+                <article className="movie-card">
+                    <a href="#" className="movie-card__poster-box">
+                        <div className="movie-card__background"
+                            style={{ backgroundImage: `url(${posterImageUrl})` }}>
+                        </div>
+                        <img src={posterImageUrl} className="movie-card__poster"></img>
+                    </a>
+                    <div className="movie-card__info-box">
+                        <h2 className="movie-card__title">
+                            <a href="#" className="link">{title}</a>
+                            <span className="movie-card__release-year">{this.getMovieReleaseYear(release_date)}</span>
+                        </h2>
+                        <div className="movie-card__genres">
+                            {genres.map(genre => (
+                                <a href="#" className="movie-card__genre" key={genre.id}>{genre.name}</a>
+                            ))}
+                        </div>
+                        <p className="movie-card__overview">{this.truncateOverviewText(150)}</p>
+                        <footer className="movie-card__footer">
+                            <p>like</p>
+                        </footer>
                     </div>
-                    <img src={`https://image.tmdb.org/t/p/w342${poster_path}`} className="movie-card__poster"></img>
-                </a>
-                <div className="movie-card__info-box">
-                    <h2 className="movie-card__title">
-                        <a href="#" className="link">{title}</a>
-                        <span className="movie-card__release-year">{this.getMovieReleaseYear(release_date)}</span>
-                    </h2>
-                    <div className="movie-card__genres">
-                        {genres.map(genre => (
-                            <a href="#" className="movie-card__genres" key={genre.id}>{genre.name}</a>
-                        ))}
-                    </div>
-                    <p className="movie-card__overview">{this.truncateOverviewText(150)}</p>
-                    <footer className="movie-card__footer">
-                        <p>like</p>
-                    </footer>
-                </div>
-            </article>
+                </article>
+            </div>
         )
     }
 }
