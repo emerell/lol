4f6ba27d1b60c75983d2287fa0dc004596b4d8fb
Lera <valeriya.mihaleva.1999@gmail.com>
2019-08-26 18:02:20 +0300

    Added validateCurrencyView() method in MovieDetails

--- src/components/movies/MovieDetails.js
+++ src/components/movies/MovieDetails.js
@@ -2,31 +2,45 @@ import React, { Component } from 'react';
 
 export class MovieDetails extends Component {
     
+    validateCurrencyView = moneyAmount => {
+        const moneyStr = (+moneyAmount).toLocaleString() 
+        const validMoneyAmount = moneyStr.split(',').join(' ')
+        return "$" + validMoneyAmount
+    }
+
     render() {
         const { genres, production_countries, runtime, budget, revenue
               } = this.props.movie;
 
         let generalInfo = {
-            // "Genres": genres,
-            // "Countries": production_countries,
+            "Genres": genres,
+            "Countries": production_countries,
             "Runtime": runtime + " min",
-            "Budget": (budget == 0 ? "No info" : budget),
-            "Revenu": (revenue == 0 ? "No info" : "$" + revenue)
+            "Budget": budget && this.validateCurrencyView(budget) || "No info" ,
+            "Revenu": revenue && this.validateCurrencyView(revenue) || "No info"
         }
-        console.log(this.props.movie)
         return (
             <section className="movie-info-section">
                 <h2 className="visually-hidden">General info</h2>
-                {
-                Object.keys(generalInfo).map((key, value) => {
-                    return <div className="movie-info-line" key={value}>
-                                <div className="movie-info-line__category">{key}</div>
-                                <div className="movie-info-line__info">
-                                    <span className="movie-info-line__tag tag"> { generalInfo[key]} </span>
+                { this.props.movie && this.props.movie.production_countries &&
+                    Object.keys(generalInfo).map((key, value) => {
+                        return <div className="movie-info-line" key={value}>
+                                    <div className="movie-info-line__category">{key}</div>
+                                    <div className="movie-info-line__info">
+                                        { Array.isArray(generalInfo[key]) ? 
+                                            generalInfo[key].map(tag => (
+                                                <span 
+                                                    className="movie-info-line__tag tag" 
+                                                    key={generalInfo[key].indexOf(tag)}
+                                                >{tag.name}</span>
+                                            ))
+                                        : 
+                                            <span className="movie-info-line__tag tag"> {generalInfo[key]} </span>
+                                        }
+                                    </div>
                                 </div>
-                            </div>
-                    })
-                }
+                        })
+                    }
             </section>
         )
     }
